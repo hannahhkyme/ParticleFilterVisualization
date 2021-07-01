e281000872305d690b23611680786eea48bb1c04
Hannah Kyme <63216601+hannahhkyme@users.noreply.github.com>
2021-07-01 15:09:02 -0700

    new repo

---
+++ ParticleFilterVisualization/ParticleFilterVisualization/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ParticleFilterVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.errorMap = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.map = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorMap)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // errorMap
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.errorMap.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.errorMap.Legends.Add(legend5);
+            this.errorMap.Location = new System.Drawing.Point(370, 0);
+            this.errorMap.Margin = new System.Windows.Forms.Padding(2);
+            this.errorMap.Name = "errorMap";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.errorMap.Series.Add(series7);
+            this.errorMap.Size = new System.Drawing.Size(378, 357);
+            this.errorMap.TabIndex = 0;
+            this.errorMap.Text = "errorMap";
+            this.errorMap.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // map
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.map.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.map.Legends.Add(legend6);
+            this.map.Location = new System.Drawing.Point(1, 0);
+            this.map.Margin = new System.Windows.Forms.Padding(2);
+            this.map.Name = "map";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series9.Legend = "Legend1";
+            series9.Name = "Series2";
+            this.map.Series.Add(series8);
+            this.map.Series.Add(series9);
+            this.map.Size = new System.Drawing.Size(365, 332);
+            this.map.TabIndex = 1;
+            this.map.Text = "map";
+            this.map.Click += new System.EventHandler(this.map_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 463);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.errorMap);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorMap)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart errorMap;
+        private System.Windows.Forms.DataVisualization.Charting.Chart map;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ParticleFilterVisualization/ParticleFilterVisualization/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace ParticleFilterVisualization
+{
+    public partial class Form1 : Form
+    {
+        private Thread cpuThread;
+        List<double>  w1xList = new List<double> ();
+        List<double> w1yList = new List<double>();
+        List<double> w2xList = new List<double>();
+        List<double> w2yList = new List<double>();
+        List<double> w3xList = new List<double>();
+        List<double> w3yList = new List<double>();
+        ParticleFilter p1 = new ParticleFilter();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void getParticleCoordinates()
+        {
+            Random random_num = new Random();
+            p1.create();
+            while (true)
+            {
+                p1.update();
+                p1.update_weights();
+                p1.correct();
+                // make coordinate list 
+                double w1 = 0.333;
+                w1xList = p1.weight_list_x(w1);
+                w1yList = p1.weight_list_x(w1);
+                double w2 = 0.666;
+                w1xList = p1.weight_list_x(w2);
+                w1yList = p1.weight_list_x(w2);
+
+                if (map.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateMap(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                if (errorMap.IsHandleCreated)
+                {
+                    this.Invoke((MethodInvoker)delegate { UpdateChart1(); });
+                }
+                else
+                {
+                    //......
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void UpdateMap()
+        {
+            map.Series["Series1"].Points.Clear();
+            map.Series["Series2"].Points.Clear();
+            for (int i = 0; i < w1xList.Count; ++i)
+            {
+                map.Series["Series1"].Points.AddXY(w1xList[i], w1yList[i]);
+                map.Series["Series2"].Points.AddXY(w2xList[i], w2yList[i]);
+            }
+        }
+
+        private void UpdateChart1()
+        {
+            errorMap.Series["Series1"].Points.Clear();
+            for (int i = 0; i < w1xList.Count; ++i)
+            {
+                errorMap.Series["Series1"].Points.AddXY(w1xList[i], w1yList[i]);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void map_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ParticleFilter p1 = new ParticleFilter();
+            p1.create();
+            System.WriteLine(p1.particleList[0].X);
+            //cpuThread = new Thread(new ThreadStart(this.getParticleCoordinates));
+            //cpuThread.IsBackground = true;
+            //cpuThread.Start();
+
+        }
+    }
+}
